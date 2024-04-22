ed93b2c46bcc71766f19e1ca0320e216520f2111
Admin-IT <Admin-IT@DESKTOP-D3KAKH2>
2024-04-22 12:45:11 +0530

    commit

---
+++ PostManagementSystem/src/main/java/com/post/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.post.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.post.entity.Post;
+
+public interface PostRepository  extends JpaRepository<Post,Integer>{
+
+	//List<Post>findByPublished(boolean published);
+	
+	//List<Post>findByPostTitle(String postName);
+	
+}
